6b78d5a4d28bdaca70a39f133d015bc3a34b65a6
Augusto Lima <augustolimads@gmail.com>
2022-02-02 22:38:07 -0300

    style(document): add project fonts
    -

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,12 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/img/icon-512.png" />
           <link rel="apple-touch-icon" href="/img/icon-512.png" />
           <link rel="manifest" href="/manifest.json" />
-          {/*add your fonts and icons here*/}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
