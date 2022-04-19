3e3431e70eabd8efdc2bb639e9ae55bc9004877f
Augusto Lima <augustolimads@gmail.com>
2022-04-19 20:31:35 -0300

    arch(stripe): add subscription endpoint
    -

--- src/components/SubscriptionButtonComponent/index.tsx
+++ src/components/SubscriptionButtonComponent/index.tsx
@@ -1,10 +1,39 @@
+import { useSession, signIn } from 'next-auth/react'
+import { api } from 'src/services/axios'
+import { getStripeJs } from 'src/services/stripe-js'
+
 type Props = {
   priceId: string
 }
 
 export const SubscriptionButtonComponent = ({ priceId }: Props) => {
+  const { data: session } = useSession()
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
+      console.log(response)
+
+      const stripe = await getStripeJs()
+
+      await stripe?.redirectToCheckout({ sessionId })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
-    <button className="bg-amber-400 text-neutral-900 rounded-full text-xl py-5 px-16 font-bold hover:brightness-75 transition-all">
+    <button
+      type="button"
+      onClick={handleSubscribe}
+      className="bg-amber-400 text-neutral-900 rounded-full text-xl py-5 px-16 font-bold hover:brightness-75 transition-all"
+    >
       Subscribe now
     </button>
   )
